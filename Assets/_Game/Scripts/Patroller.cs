db8d82c298ae8f4b707ea9975737396d3785c54b
Jenny <cottonjenny3@hotmail.com>
2023-12-01 16:31:52 -0600

    Update Patrolling Script and Prefabs
    Update patrolling script to make enemy look at target point.
    update spring prefab to make sure player can walk over flowers. put foliage in appropriate separator.
    create base for seasonal prefabs

--- Assets/_Game/Scripts/Patroller.cs
+++ Assets/_Game/Scripts/Patroller.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Patroller : MonoBehaviour
 {
+    [Header("Patrolling")]
     public float _enemySpeed = 1;
 
     public Transform _point1;
@@ -16,12 +17,33 @@ public class Patroller : MonoBehaviour
         while (true)
         {
             transform.position = Vector3.MoveTowards(transform.position, target.position, _enemySpeed * Time.deltaTime);
+            transform.LookAt(target);
             if (Vector3.Distance(transform.position, target.position) <= 0)
             {
                 target = target == _point1 ? _point2 : _point1;
                 yield return new WaitForSeconds(0.5f);
             }
             yield return null;
+        }
+    }
+
+    //when colliding with player
+    [Header("Collision With Player")]
+    [SerializeField] AudioClip _hurt;
+    private LevelController _lvlController;
+
+    private void Awake()
+    {
+        _lvlController = GameObject.FindGameObjectWithTag("LevelController").GetComponent<LevelController>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            AudioHelper.PlayClip2D(_hurt, .2f);
+
+            _lvlController.Lose();
         }
     }
 }
